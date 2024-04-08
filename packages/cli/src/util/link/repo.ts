6155759e400c7cc7fea86ac284c77da55a171789
Nathan Rajlich <n@n8.io>
2024-04-08 11:35:37 -0700

    Revert "[cli] Fix how we determine the GIT_CONFIG_PATH to support git worktrees and git submodules (#11283)"
    This reverts commit 5b8b87739e205850ad438b2e1d7de1e9ecdd53e6.

--- packages/cli/src/util/link/repo.ts
+++ packages/cli/src/util/link/repo.ts
@@ -22,7 +22,6 @@ import { detectProjects } from '../projects/detect-projects';
 import { repoInfoToUrl } from '../git/repo-info-to-url';
 import { connectGitProvider, parseRepoUrl } from '../git/connect-git-provider';
 import { isAPIError } from '../errors-ts';
-import { isGitWorktreeOrSubmodule } from '../git-helpers';
 
 const home = homedir();
 
@@ -360,13 +359,7 @@ export async function findRepoRoot(
 ): Promise<string | undefined> {
   const { debug } = client.output;
   const REPO_JSON_PATH = join(VERCEL_DIR, VERCEL_DIR_REPO);
-  /**
-   * If the current repo is a git submodule or git worktree '.git' is a file
-   * with a pointer to the "parent" git repository instead of a directory.
-   */
-  const GIT_PATH = isGitWorktreeOrSubmodule({ cwd: client.cwd })
-    ? normalize('.git')
-    : normalize('.git/config');
+  const GIT_CONFIG_PATH = normalize('.git/config');
 
   for (const current of traverseUpDirectories({ start })) {
     if (current === home) {
@@ -390,12 +383,12 @@ export async function findRepoRoot(
 
     // if `.git/config` exists (unlinked),
     // then consider this the repo root
-    const gitConfigPath = join(current, GIT_PATH);
+    const gitConfigPath = join(current, GIT_CONFIG_PATH);
     stat = await lstat(gitConfigPath).catch(err => {
       if (err.code !== 'ENOENT') throw err;
     });
     if (stat) {
-      debug(`Found "${GIT_PATH}" - detected "${current}" as repo root`);
+      debug(`Found "${GIT_CONFIG_PATH}" - detected "${current}" as repo root`);
       return current;
     }
   }
